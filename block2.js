e5ef98dc7ccd8d471e4c76991c3d1f80e70ad19f
tanush4 <73825544+tanush4@users.noreply.github.com>
2020-12-14 21:15:34 +0530

    Add files via upload

---
+++ block2.js
@@ -0,0 +1,20 @@
+class Block2  {
+    constructor(x,y,width,height)  {
+        var options={
+            isStatic:false
+        }
+        this.width = width
+        this.height = height
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        
+
+        World.add(world,this.body)
+
+    }
+    display()  {
+        rectMode(CENTER)
+        fill("purple")
+        rect(this.body.position.x,this.body.position.y,this.width,this.height)
+
+    }
+}
